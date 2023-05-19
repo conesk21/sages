72e876905e3da8e1c2a3694ee068ebede24449a0
conesk21 <katie.s.cones@outlook.com>
2023-05-19 10:00:37 +0300

    add Display child to Card to handle the discount/markup feature independently

--- src/Card.js
+++ src/Card.js
@@ -1,36 +1,15 @@
 import React,{Component} from "react";
 import { fantasy } from "./classes.js";
 
-class Card extends Component{
+class Display extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            item: props.item,
-            value: 1,
-            view: "static",
-            price: fantasy.getCoins(props.item.getValue()),
-            discount: {value: 10, price: fantasy.getCoins(props.item.getValue()*.9)},
-            markup: {value: 10, price: fantasy.getCoins(props.item.getValue()*1.1)},
+            value: props.value,
+            discount: {value: 10, price: fantasy.getCoins( props.value*.9)},
+            markup: {value: 10, price: fantasy.getCoins( props.value*1.1)},
         };
     }
-    handleInput = (event) =>{
-        let val = event.target.value/100
-        let itemVal = this.state.item.getValue()
-        this.setState(
-            { value: val,
-                price: fantasy.getCoins(val*itemVal)}
-        )
-    }
-    changeView = (e) =>{
-        var ne;
-        this.state.view === "edit"? ne="static": ne = "edit"
-        this.setState({
-            view: ne
-            });
-            
-        
-    }
-
     changeDiscount = (e) =>{
         if (e.target.value.toString().length===0){
             e.target.style.width = 2 + "ch"; 
@@ -42,11 +21,12 @@ class Card extends Component{
 
         this.setState(
             {
-                discount:{value: e.target.value, price: fantasy.getCoins(this.state.item.getValue()*dis)}
+                discount:{value: e.target.value, price: fantasy.getCoins(this.state.value*dis)}
             }
         )
         
     }
+
     changeMarkup = (e) =>{
         if (e.target.value.toString().length===0){
             e.target.style.width = 2 + "ch"; 
@@ -57,33 +37,56 @@ class Card extends Component{
 
         this.setState(
             {
-                markup:{value: e.target.value, price: fantasy.getCoins(this.state.item.getValue()*mar)}
+                markup:{value: e.target.value, price: fantasy.getCoins(this.state.value*mar)}
             }
         )
         
     }
-
     render(){
-        let middle; 
-        if (this.state.view === "edit") {
-            middle = <div>
-                <div className="item-var"> <span><input type="number"   placeholder={this.state.discount.value} onChange={this.changeDiscount}></input>% discount: </span> <p>{this.state.discount.price}</p></div>
-            <div className="item-var"> <span><input type="number" placeholder={this.state.markup.value} onChange={this.changeMarkup}></input>% markup: </span> <p>{this.state.markup.price}</p></div>
+        return(
+            <div>
+            <div className="item-var"> <span><input type="number"   value={this.state.discount.value} onChange={this.changeDiscount}></input>% discount: </span> <p>{this.state.discount.price}</p></div>
+            <div className="item-var"> <span><input type="number" value={this.state.markup.value} onChange={this.changeMarkup}></input>% markup: </span> <p>{this.state.markup.price}</p></div>
                
             </div> 
 
-        } 
+        )
+    }
+}
+
+class Card extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: props.item,
+            view: "static",
+            price: fantasy.getCoins(props.item.getValue()),
+        };
+    }
+    
+    changeView = (e) =>{
+        var ne;
+        this.state.view === "edit"? ne="static": ne = "edit"
+        this.setState({
+            view: ne
+            });
+            
+        
+    }
+
+    render(){
+
     return (
         <div className="item-card">
             <div className="item-title">
             <h3>{this.state.item.name}</h3>
-            <button className="item-more" onClick={this.changeView}><span class="material-symbols-outlined">
+            <button className="item-more" onClick={this.changeView}><span className="material-symbols-outlined">
 more_horiz
 </span></button>
             </div>
             
             
-            {middle}
+            {this.state.view === "edit" && <Display value={this.state.item.getValue()}/> }
             <div className="item-price">
              <span>{this.state.price}</span>   
             </div>
