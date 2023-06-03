92fe13c51896e8d98d43b9e0e5c9606785dce8a8
conesk21 <katie.s.cones@outlook.com>
2023-06-03 13:25:06 +0300

    add functionality to CurrencyForm allowing for edit/removal of coins

--- src/CurrencyForm.js
+++ src/CurrencyForm.js
@@ -49,49 +49,150 @@ class Preveiw extends Component{
 }
 
 class Names extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: false
+    };
+  }
+  switchTemp=()=>{
+    this.setState({
+      temp: !this.state.temp
+    })
+  }
+
+  onSave=(oldName, newName)=>{
+    this.props.onSave(oldName,newName)
+    this.setState({
+      temp:false
+    })
+
+  }
+  
+  
+
   render(){
     return(
       <div>
-        
+        <p>input the names of each coin (order doesn't matter)</p>
+        <button type="button" onClick={this.switchTemp}>add coin</button>
+        {this.state.temp && <CoinCard name={""} display={false} save={this.onSave} revert={this.switchTemp}/>}
+        {this.props.names.map((item)=>{
+          return <CoinCard name={item} display={true} save={this.onSave} revert={this.switchTemp} delete={this.props.onRemove}/>
+        })}
+
+        <p>primary coin:</p>
+        <select onChange={this.props.onPrimaryChange}>
+          {this.props.names.map((item)=>{
+          return <option key={item}>{item}</option>
+        })}</select>
       </div>
     )
   }
 }
 
+class CoinCard extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: this.props.name,
+      display: this.props.display
+    };
+  }
+
+  onChange=(e)=>{
+    this.setState({
+      temp: e.target.value
+    })
+  }
+  changeDisplay = (e) =>{
+    this.setState({
+      display: !this.state.display
+    })
+  }
+
+  onRevert=()=>{
+    this.props.revert()
+    this.setState({
+      temp: this.props.name, 
+      display: true
+    })
+  }
+  onSave=()=>{
+    this.setState({
+      display: true
+    })
+    this.props.save(this.props.name, this.state.temp)}
+  
+
+  render(){
+
+    var title = <div>
+      <p onClick={this.changeDisplay}>{this.props.name}</p> 
+      <button type="button" onClick={()=>{this.props.delete(this.props.name)}}>delete</button>
+    </div>
+    var input =<div>
+      <input type="text" value={this.state.temp} onChange={this.onChange}></input>
+      <button type="button"onClick={this.onRevert}>X</button> 
+      <button type="button"onClick={this.onSave}>+</button> 
+    </div> 
+    return(
+      <div>
+        {this.state.display ? title:input}</div>
+     
+
+    )
+  }
+
+}
 
 
 class CurrencyForm extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      temp:"",
       primary: "",
       currency: {}
     };
   }
-    onNameChange=(e)=>{
-      this.setState({
-        temp: e.target.value
-      })
-    }
-    addName =()=>{
-      var nam = this.state.temp
-      var coin = {...this.state.currency}
-      coin[nam] = 1
+    
+    addName =(newName)=>{
+      var coins = {...this.state.currency}
+      coins[newName] = 1
       if (Object.keys(this.state.currency).length === 0){
         this.setState({
-          temp: "",
-          currency: coin,
-          primary: this.state.temp
+          currency: coins,
+          primary: newName
           })
       } else {
         this.setState({
-        temp: "",
-        currency: coin,
+        currency: coins,
         })
       }
       
     }
+    changeName=(oldName, newName)=>{
+      if(oldName===""){
+        this.addName(newName)
+      } else{
+        var coins = {...this.state.currency}
+        coins[newName] = coins[oldName]
+        delete coins[oldName]
+        this.setState({
+          currency: coins
+        })
+
+      }
+    }
+
+    deleteName=(name)=>{
+      var coin = {...this.state.currency}
+      delete coin[name] 
+      this.setState({
+        currency: coin
+      })
+    }
+
     onPrimaryChange=(e)=>{
       this.setState({
         primary: e.target.value
@@ -113,13 +214,7 @@ class CurrencyForm extends Component{
       <form className="currency-form">
         <h1>Change Currency</h1>
         <h4>Denominations</h4>
-        <p>input the names of each coin (order)</p>
-        <input type="text" value={this.state.temp} onChange={this.onNameChange}></input> < button type="button" onClick={this.addName}>add denomination</button>
-        <p>primary coin:</p>
-        <select onChange={this.onPrimaryChange}>
-          {Object.keys(this.state.currency).map((item)=>{
-          return <option key={item}>{item}</option>
-        })}</select>
+        <Names names={Object.keys(this.state.currency)} onSave={this.changeName} onPrimaryChange={this.onPrimaryChange} onRemove={this.deleteName}/>
         <h4>Conversions</h4>
         {Object.keys(this.state.currency).filter((item)=> item !== this.state.primary).map((name)=>{
           return <Conversion coin={name} primary={this.state.primary} onChange={this.updateObject}/>
